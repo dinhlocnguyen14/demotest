d0f0e30bcd53291b8abf7c1cb234f5a3b279bf59
DinhLoc <dinhlocnguyen601@gmail.com>
2026-04-14 21:19:06 +0700

    feat: add app

---
+++ mobile/app/(auth)/verify-email.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const VerifiEmailScreen = () => {
+  return (
+    <View>
+      <Text>VerifiEmailScreen</Text>
+    </View>
+  );
+};
+
+export default VerifiEmailScreen;
